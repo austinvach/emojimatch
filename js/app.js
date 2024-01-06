3b0a3088efb6ab1a5bab01b3848cffae88945bf1
Austin Vach <austinvach@outlook.com>
2024-01-05 23:16:46 -0800

    Adding all fluent emoji.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 document.addEventListener("click", changeSVGOnClick);
 
 function changeSVGOnClick(event) {
-    event.target.src = "media/open_mailbox_with_raised_flag_flat.svg";
+    event.target.src = "media/emoji/open_mailbox_with_raised_flag_flat.svg";
 }
